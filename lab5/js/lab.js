55f3b9b1c52ec983911b33c2002c87bf917ec3d7
angrysnort <151792710+angrysnort@users.noreply.github.com>
2024-10-21 18:46:55 -0700

    mlvnsdnfdlsnfsknfsked

--- lab5/js/lab.js
+++ lab5/js/lab.js
@@ -1,10 +1,23 @@
-// index.js - purpose and description here
-// Author: Your Name
-// Date:
+// Corbin Wilhelm <ljwilhel@ucsc.edu>
+// Lucas Larkin <djlarkin@ucsc.edu>
+// October 2024
+// License: Public Domain
 
-// Constants
+// Define Variables
+make = "Cadillac";
+model = "Hearse";
+color = "Black";
+year = "1970";
 
-// Functions
+// Calculate
+age = 2024 - year;
+
+// Output
+document.writeln("Make: " + make + "<br>");
+document.writeln("Model: " + model + "<br>");
+document.writeln("Color: " + color + "<br>");
+document.writeln("Year: " + year + "<br>");
+document.writeln("Age: " + age + "<br>");
 
 // this is an example function and this comment tells what it doees and what parameters are passed to it.
 function myFunction(param1, param2) {

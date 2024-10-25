81b86758726d97e5996e399e811d221c16f861e7
angrysnort <151792710+angrysnort@users.noreply.github.com>
2024-10-24 18:38:08 -0700

    waesrdtfyguhjikol;'

--- lab6/js/lab.js
+++ lab6/js/lab.js
@@ -1,21 +1,35 @@
-// index.js - purpose and description here
-// Author: Your Name
-// Date:
+// Corbin Wilhelm <ljwilhel@ucsc.edu>
+// Lucas Larkin <djlarkin@ucsc.edu>
+// October 2024
+// License: Public Domain
 
 // Constants
+const myTransport = ["The Bus" , "Dream Car: The Cadillac Hearse"] 
 
-// Functions
+var myMainButAlsoDreamRide = {
+  make: "Cadillac",
+  model: "Hearse",
+  color: "Black",
+  year: 1970,
+  age: function() {
+      return 2024 - this.year;
+  }
+}
+
+document.writeln("I only use two kinds of transport: " + myTransport[0] + " and my " + myTransport[1] + "</br>");
+document.writeln("Here's the details for My Dream Ride, which I hope to someday be My Main Ride: <pre></br>", 
+  JSON.stringify(myMainButAlsoDreamRide, null, '\t'), "</pre></br>");
 
 // this is an example function and this comment tells what it doees and what parameters are passed to it.
-function myFunction(param1, param2) {
+//function myFunction(param1, param2) {
   // some code here
   // return results;
-}
+//}
 
-function main() {
-  console.log("Main function started.");
+//function main() {
+  //console.log("Main function started.");
   // the code that makes everything happen
-}
+//}
 
 // let's get this party started
-main();
+//main();

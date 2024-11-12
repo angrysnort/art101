197c02dcc0c0e82e36ea2be256cd3a5477487fbf
angrysnort <151792710+angrysnort@users.noreply.github.com>
2024-11-11 21:26:19 -0800

    asdasdasdas

--- lab11/js/lab.js
+++ lab11/js/lab.js
@@ -1,21 +1,21 @@
-// index.js - purpose and description here
-// Author: Your Name
-// Date:
+// Corbin Wilhelm <ljwilhel@ucsc.edu>
+// Lucas Larkin <djlarkin@ucsc.edu>
+// November 2024
+// License: Public Domain
 
-// Constants
-
-// Functions
-
-// this is an example function and this comment tells what it doees and what parameters are passed to it.
-function myFunction(param1, param2) {
-  // some code here
-  // return results;
+// Sorts the characters of a string in alphabetical order.
+function sortString(inputString) {
+  // We have to convert our string to an array and back again to sort it
+  return inputString.split('').sort().join('');
 }
 
-function main() {
-  console.log("Main function started.");
-  // the code that makes everything happen
-}
-
-// let's get this party started
-main();
+// click listener for button
+$("#submit").click(function(){
+  // get value of input field
+  const userName = $("#user-name").val();
+  // now let's sort it
+  userNameSorted = sortString(userName);
+  // append a new div to our output div
+  //I chose to prepend it to a div that lies within the output div in order to get the most recent name appear closest to the input box. 
+  $("#sorted-name").prepend('<div class="text"><p>' + "Here's your sorted username: " + userNameSorted + '</p></div>');
+});
